3347311c6a98157b7d65f650775744e7f48af637
dubidubob <87163850+dubidubob@users.noreply.github.com>
2023-06-03 02:09:51 +0900

    헤에..

--- 3phase new/Assets/MoveToTarget.cs
+++ 3phase new/Assets/MoveToTarget.cs
@@ -7,6 +7,7 @@ public class MoveToTarget : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition.transform.position, 0.005f);
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition.transform.position, 0.001f);
+        
     }
 }
---
+++ 3phase new/Assets/collision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.CompareTag("target"))
+        {
+            Debug.Log("This is target");
+        }
+    }
+}
